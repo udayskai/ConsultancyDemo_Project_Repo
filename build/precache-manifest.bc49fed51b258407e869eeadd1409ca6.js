e21c04921cee4e844bdd451b2cb25a376e35dba7
Udayskai <udayskai@gmail.com>
2020-04-14 04:03:11 +0530

    add  build root

---
+++ build/precache-manifest.bc49fed51b258407e869eeadd1409ca6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3a66bbb9d8c17ed79f3a33a4cedacb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92c0e529426ecbe4b1f9",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f9a5e00106ca5ce82870",
+    "url": "/static/css/main.85e98047.chunk.css"
+  },
+  {
+    "revision": "92c0e529426ecbe4b1f9",
+    "url": "/static/js/2.403654dc.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.403654dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a5e00106ca5ce82870",
+    "url": "/static/js/main.47d28f6e.chunk.js"
+  },
+  {
+    "revision": "9a1557eb766707c487e2",
+    "url": "/static/js/runtime-main.d709b0e7.js"
+  },
+  {
+    "revision": "1c9022fab3a515ac6c723d27268d2776",
+    "url": "/static/media/2.1c9022fa.jpg"
+  }
+]);
